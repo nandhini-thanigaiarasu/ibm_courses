740880ef099c0bae718340346ac51d72a49ec698
nandhini-thanigaiarasu <nandhini.t.arasu@gmail.com>
2025-11-22 08:06:24 -0500

    weather report

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -9,7 +9,7 @@ function showweatherDetails(event) {
         .then(data => {
           const weatherInfo = document.getElementById('weatherInfo');
           weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
-                                  <p>Temperature: ${data.main.temp} &#8451;</p>
+                                  <p>Temperature: ${data.main.temp}</p>
                                   <p>Weather: ${data.weather[0].description}</p>
                                   <p>Weather updates finished!...</p>`;
         })
@@ -23,4 +23,31 @@ function showweatherDetails(event) {
 
     }
 
-    document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+    document.getElementById('weatherForm').addEventListener('submit',showweatherDetails);
+    document.getElementById('weatherForm2').addEventListener('submit',showweatherDetails2);
+
+    function showweatherDetails2(event) {
+        event.preventDefault();
+         const lat = document.getElementById('latitude').value;
+         const lon = document.getElementById('longitude').value;
+        const apiKey = '6050077fe121afb50c015d203d9e6fbb'; // Replace 'YOUR_API_KEY' with your actual API key
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  
+         fetch(apiUrl)
+          .then(response => response.json())
+          .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                    <p>Temperature: ${data.main.temp}</p>
+                                    <p>Weather: ${data.weather[0].description}</p>
+                                    <p>Weather updates finished!...</p>`;
+          })
+          .catch(error => {
+              console.error('Error fetching weather:', error);
+              const weatherInfo = document.getElementById('weatherInfo');
+              weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+            });
+  
+  
+  
+      }
